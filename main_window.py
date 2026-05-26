8eb6e9a77e57d1526b54552bfd66d9701f301d43
huangqiliang <goosehuangmatt@gmail.com>
2026-05-26 14:38:27 +0800

    Refine separator dimensions and margins for enhanced UI consistency
    - Reduced the fixed width of the _FlowSeparator from 6px to 2px for improved visual alignment.
    - Updated QToolBar::separator margins from 2px to 0px, creating a tighter and more cohesive appearance across the toolbar.
    - Adjusted margin settings for various elements in the MainWindow to ensure consistent spacing.
    
    These changes contribute to a more polished and visually appealing user interface by ensuring uniformity in separator dimensions and spacing.

--- main_window.py
+++ main_window.py
@@ -2151,7 +2151,7 @@ class _FlowSeparator(QWidget):
     def __init__(self, parent=None, color="#3d3d5c"):
         super().__init__(parent)
         self.setObjectName("_flow_separator")
-        self.setFixedWidth(6)  # 线居中，两侧由 FlowLayout 的 h_spacing 提供对称间距
+        self.setFixedWidth(2)  # 线居中，两侧由 FlowLayout 的 h_spacing 提供对称间距
         self.setFixedHeight(22)
         self._color = QColor(color)
 
@@ -2728,8 +2728,8 @@ class MainWindow(QMainWindow):
             QToolBar::separator {
                 background-color: #3d3d5c;
                 width: 1px;
-                margin-left: 2px;
-                margin-right: 2px;
+                margin-left: 0px;
+                margin-right: 0px;
                 margin-top: 6px;
                 margin-bottom: 6px;
             }
@@ -2843,7 +2843,7 @@ class MainWindow(QMainWindow):
                 background-color: #0f1626;
                 color: #888;
                 padding: 7px 18px;
-                margin-right: 2px;
+                margin-right: 0px;
                 margin-top: 3px;
                 border-top-left-radius: 4px;
                 border-top-right-radius: 4px;
@@ -7401,8 +7401,8 @@ class MainWindow(QMainWindow):
             QToolBar::separator {{
                 background-color: {t['border']};
                 width: 1px;
-                margin-left: 2px;
-                margin-right: 2px;
+                margin-left: 0px;
+                margin-right: 0px;
                 margin-top: 6px;
                 margin-bottom: 6px;
             }}
@@ -7505,7 +7505,7 @@ class MainWindow(QMainWindow):
                 background-color: {t['bg_medium']};
                 color: {t['text_dim']};
                 padding: 7px 18px;
-                margin-right: 2px;
+                margin-right: 0px;
                 margin-top: 3px;
                 border-top-left-radius: 4px;
                 border-top-right-radius: 4px;
@@ -7548,8 +7548,8 @@ class MainWindow(QMainWindow):
             QToolBar::separator {{
                 background-color: {t['border']};
                 width: 1px;
-                margin-left: 2px;
-                margin-right: 2px;
+                margin-left: 0px;
+                margin-right: 0px;
                 margin-top: 5px;
                 margin-bottom: 5px;
             }}
